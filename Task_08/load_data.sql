3221974e4d4b0f8a2dca70a54875bb0248244c75
dsermolin <dsermolin@fintechiq.ru>
2022-06-14 22:12:33 +0700

    add readme to task 09 and files to load in smashup database

---
+++ Task_08/load_data.sql
@@ -0,0 +1,42 @@
+--созданю схему и три временные таблицы
+create schema ur;
+
+create table if not exists ur.bases
+(
+    id           int2,
+    name         varchar(50),
+    properties   varchar(2000),
+    health       int2,
+    first_place  int2,
+    second_place int2,
+    third_place  int2
+);
+\copy ur.bases FROM '/var/lib/postgresql/data/pgdata/csv_data/bases.csv' DELIMITER ';' CSV HEADER;
+
+create table if not exists ur.actions
+(
+    id            int2,
+    fractions     varchar(20),
+    name          varchar(20),
+    description   varchar(2000),
+    type_action   varchar(20),
+    count_in_desc int2
+);
+\copy ur.bases FROM '/var/lib/postgresql/data/pgdata/csv_data/action.csv' DELIMITER ';' CSV HEADER;
+
+create table if not exists ur.minions
+(
+    id            int2,
+    fractions     varchar(20),
+    name          varchar(30),
+    properties    varchar(2000),
+    action_type   varchar(20),
+    attack        int2,
+    count_in_desc int2
+);
+\copy ur.bases FROM '/var/lib/postgresql/data/pgdata/csv_data/action.csv' DELIMITER ';' CSV HEADER;
+
+drop table ur.bases;
+
+select *
+from ur.bases;
---
+++ Task_09/ReadMe.md
@@ -0,0 +1,48 @@
+# DML: вставка, обновление, удаление, выборка данных 
+
+### Подготовка справочников
+Сначала загружаю данные в ранее созданные таблицы, по сути заполняю справочники, в схеме **dc**.
+
+Заранее подготовил три файла: 
+
+(приложить здесь файлы)
+
+Для них создал три временные таблицы, из которых потом буду раскидывать по справочникам:
+- bases
+```
+create table if not exists ur.bases
+(
+    id           int2,
+    name         varchar(50),
+    properties   varchar(2000),
+    health       int2,
+    first_place  int2,
+    second_place int2,
+    third_place  int2
+);
+```
+- actions
+```
+create table if not exists ur.actions
+(
+    id            int2,
+    fractions     varchar(20),
+    name          varchar(20),
+    description   varchar(2000),
+    type_action   varchar(20),
+    count_in_desc int2
+);
+```
+- minions
+```
+create table if not exists ur.minions
+(
+    id            int2,
+    fractions     varchar(20),
+    name          varchar(30),
+    properties    varchar(2000),
+    action_type   varchar(20),
+    attack        int2,
+    count_in_desc int2
+);
+```
